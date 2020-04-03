9831aab5c6e25e293198fdb91ac4f4528b8552f1
8.1 <8.1@sony>
2020-04-03 21:50:53 +0530

    ui dao exception service bean packages created
    Student class created under bean

---
+++ UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/App.java
@@ -0,0 +1,13 @@
+package com.cg.UniversityAdmissionSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UniversityAdmissionSystem/src/main/java/com/cg/UniversityAdmissionSystem/bean/Student.java
@@ -0,0 +1,5 @@
+package com.cg.UniversityAdmissionSystem.bean;
+
+public class Student {
+
+}
